ba9c0696a9ba2775de0487879ab282f334523c64
Richard Vega <121314810+prosecutorph@users.noreply.github.com>
2023-01-03 03:29:59 +0800

    Update track.py

--- track.py
+++ track.py
@@ -1,58 +1,39 @@
+import pymotion as pm
 import cv2
-import numpy as np
 
 # Set up the camera
 camera = cv2.VideoCapture(0)
 
-# Capture the first frame
-_, frame = camera.read()
-prev_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-# Set up the feature points
-feature_params = dict(maxCorners=100, qualityLevel=0.3, minDistance=7, blockSize=7)
-features = cv2.goodFeaturesToTrack(prev_gray, mask=None, **feature_params)
-
-# Create a mask image for drawing purposes
-mask = np.zeros_like(frame)
-
-# Define the parameters for the Lucas-Kanade optical flow algorithm
-lk_params = dict(winSize=(15, 15), maxLevel=2, criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+# Create an OpenCV background subtractor
+subtractor = cv2.createBackgroundSubtractorMOG2()
 
 # Capture and display the frames from the camera
 while True:
     # Capture the frame
     _, frame = camera.read()
 
-    # Convert the frame to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Apply the background subtractor to the frame
+    mask = subtractor.apply(frame)
 
-    # Calculate the optical flow
-    new_features, status, error = cv2.calcOpticalFlowPyrLK(prev_gray, gray, features, None, **lk_params)
+    # Find contours in the mask
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Select the good points
-    good_features = features[np.expand_dims(status, axis=1) == 1]
-    new_features = new_features[np.expand_dims(status, axis=1) == 1]
+    # Find the largest contour
+    if contours:
+        c = max(contours, key=cv2.contourArea)
+        x, y, w, h = cv2.boundingRect(c)
 
-
-    # Draw the tracks
-    for i, (new, old) in enumerate(zip(new_features, good_features)):
-        a, b = new.ravel()
-        c, d = old.ravel()
-        mask = cv2.line(mask, (int(a), int(b)), (int(c), int(d)), (0, 255, 0), 2)
-        frame = cv2.circle(frame, (int(a), int(b)), 5, (0, 0, 255), -1)
+        # Draw a rectangle around the contour
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     # Display the frame
     cv2.imshow("Camera", frame)
 
-    # Update the previous frame and the feature points
-    prev_gray = gray
-    features = new_features
-
-    # Check if the 'q' key has been pressed
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord("q"):
+    # Check for user input
+    key = cv2.waitKey(1)
+    if key == 27:  # Press 'ESC' to quit
         break
 
-# Release the camera and destroy the windows
+# Clean up
 camera.release()
 cv2.destroyAllWindows()
